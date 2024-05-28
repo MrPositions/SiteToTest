2340b1809adafd86963624399295a8fb999ddafd
MrPositions <dotasmurf4444@gmail.com>
2024-05-28 20:14:58 +0500

    a

--- functionality.js
+++ functionality.js
@@ -9,11 +9,3 @@ function changeImg() {
 let imgLink = this.getAttribute("data-img")
 sideImg.setAttribute("src", imgLink)
 }
-document.getElementById('menu-icon').addEventListener('click', function() {
-      var menu = document.getElementById('menu');
-      if (menu.style.display === 'none') {
-        menu.style.display = 'block';
-      } else {
-        menu.style.display = 'none';
-      }
-});
